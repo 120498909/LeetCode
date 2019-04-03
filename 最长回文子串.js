3a5df112f87cc4068f37992d33bded7392c35ec7
liuzhan <120498909@qq.com>
2019-04-03 22:18:28 +0800

    最长回文

--- 最长回文子串.js
+++ 最长回文子串.js
@@ -1,3 +1,4 @@
+//O(n3) 的暴力解法
 var longestPalindrome = function(s) {
   let max = 0
   let tmpMax = 0
@@ -30,5 +31,47 @@ var longestPalindrome = function(s) {
   return maxStr
 };
 
-let s= "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
-console.log(longestPalindrome(s))
+//动态规划
+let longestPalindrome = (s) => {
+  if (s === '' || s.length < 1) return ''
+  let str = s.split('')
+  let res = new Array(str.length)
+  let max = 0
+  let maxi = 0
+  let maxj = 0
+  for (let i = 0; i < str.length; i++) {     //初始化
+    res[i] = new Array(str.length)
+    for (let j = 0; j < str.length; j++) {
+      if (i === j) {
+        res[i][j] = true
+      } else {
+        res[i][j] = false
+      }
+    }
+  }
+  for (let j = 0; j < str.length; j++) {
+      for (let i = 0; i < j; i++) {
+      if(i + 1 === j && str[i] === str[j]){
+        res[i][j] = true
+        if(max < j - i + 1 ) {
+          max = j - i + 1
+          maxi = i
+          maxj = j
+        }
+      }
+      if(res[i + 1][j - 1] === true && str[i] === str[j]){
+        res[i][j] = true
+        if(max < j - i + 1 ) {
+          max = j - i + 1
+          maxi = i
+          maxj = j
+        }
+      }
+    }
+  }
+  let sub = str.slice(maxi, maxj + 1).join('')
+  console.log(maxi, maxj)
+}
+// let s= "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+// // console.log(longestPalindrome(s))
+longestPalindrome(s)
