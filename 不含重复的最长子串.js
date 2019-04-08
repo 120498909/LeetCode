eb112bfc2c79962eddda7fe3c4739c544b374b1d
liuzhan <120498909@qq.com>
2019-04-08 22:37:54 +0800

    最大子序和

--- 不含重复的最长子串.js
+++ 不含重复的最长子串.js
@@ -22,3 +22,36 @@ let lengthOfLongestSubstring = (s) => {
 
 let s = '1'
 console.log(lengthOfLongestSubstring(s))
+
+var readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal:false
+});
+
+// rl.on('line', function(line){ // javascript每行数据的回调接口
+//   if(line === '') {  //边界条件
+//     console.log(0)
+//     return
+//   }
+//   if(line.length === 1) {  //边界条件
+//     console.log(1)
+//     return
+//   }
+//   let str = line.split('')
+//   let sub = ''
+//   let max = 0
+//   for(let i = 0; i < line.length; i++) {  //该重循环控制当前子串的第一个字符
+//     for(let j = i; j < line.length; j++) {  //该重循环为第一个字符确定后，子串的长度
+//       if(sub.indexOf(str[j]) === -1) {  //如果str[j]不在sub中，即不重复，就将其放入sub中
+//         sub += str[j]
+//       } else {  //否则，计算当前无重复子串长度，与max比较，较大者来更新max
+//         max = max > sub.length ? max : sub.length
+//         break
+//       }
+//     }
+//     sub = ''  //每重循环结束，sub清零
+//   }
+//   console.log(max)
+// });
